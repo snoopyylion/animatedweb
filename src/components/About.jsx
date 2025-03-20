7ad9b0b1e78ebf4e9e7e1ade2a4cdc7bda0e15f6
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-03-20 16:47:25 +0100

    updated feature

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,47 +1,63 @@
-import { useGSAP } from '@gsap/react'
-import React from 'react'
-import gsap from 'gsap'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
 
-import { ScrollTrigger } from 'gsap/all'
-import AnimatedTitle from './AnimatedTitle';
+import AnimatedTitle from "./AnimatedTitle";
+
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
-    useGSAP(()=>{
-        const clipAnimation = gsap.timeline({
-            scrollTrigger:{
-               trigger: '#clip',
-               start: 'center center',
-               end: '+=800 center',
-               scrub: 0.5,
-               pin: true,
-               pinSpacing: true,
-            }
-        }) 
-        clipAnimation.to('.mask-clip-path',{
-            width: '100vw',
-            height: '100vh',
-            borderRadius: 0
-        })
-    })
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
   return (
-    <div id='about' className='min-h-screen w-screen'>
-        <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-            <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Hojo</h2>
-            <AnimatedTitle title="Disc<b>o</b>ver the world's  <b>largest</b> shared <b>adventure</b>" containerClass="mt-5 !text-black text-center"/>
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </p>
 
-            <div className='about-subtext' >
-                <p>The Game of Games begins-your life, now an epic MMORPG</p>
-                <p>Hojo unites every player in a shared experience of adventure</p>
-            </div>
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
-        <div className='h-dvh w-screen' id="clip">
-            <div className='mask-clip-path about-image'>
-                <img src="img/about.webp" className='absolute left-0 top-0 size-full object-cover' alt="background" />
-            </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
