f500b263e44e97158fe939d80422efec99ea9ef7
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-03-20 13:21:47 +0100

    updated work

---
+++ src/components/Features.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>
+        <section className='bg-black pb-52 h-dvh w-dvw'>
+            <div className='container mx-auto'>
+
+            </div>
+
+        </section>
+    </div>
+  )
+}
+
+export default Features
